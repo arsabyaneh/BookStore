7c6cef58d68da62371fe56ed12d480d9715a933e
arsabyaneh <63109912+arsabyaneh@users.noreply.github.com>
2022-10-22 21:39:48 +0200

    added Category model; added connection string and database migration; added a simple controller for category

---
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BookStoreWeb.Data;
+using BookStoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _dbContext.Categories;
+
+            return View(categories);
+        }
+    }
+}
