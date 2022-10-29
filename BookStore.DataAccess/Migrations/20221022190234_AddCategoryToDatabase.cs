e694ac32265ca9fae33d415363f74606cbe9f397
arsabyaneh <63109912+arsabyaneh@users.noreply.github.com>
2022-10-29 16:50:11 +0200

    added data acces and model projects

--- BookStore.DataAccess/Migrations/20221022190234_AddCategoryToDatabase.cs
+++ BookStore.DataAccess/Migrations/20221022190234_AddCategoryToDatabase.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace BookStoreWeb.Migrations
+namespace BookStore.DataAccess.Migrations
 {
     public partial class AddCategoryToDatabase : Migration
     {
--- BookStore.Models/Category.cs
+++ BookStore.Models/Category.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace BookStoreWeb.Models
+namespace BookStore.Models
 {
     public class Category
     {
