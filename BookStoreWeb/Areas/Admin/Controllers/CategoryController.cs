f88681818fcab77146e494661aea52f7ea4c4a09
arsabyaneh <63109912+arsabyaneh@users.noreply.github.com>
2022-10-29 23:44:27 +0200

    Areas in action

--- BookStoreWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BookStoreWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -2,8 +2,9 @@
 using BookStore.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BookStoreWeb.Controllers
+namespace BookStoreWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
