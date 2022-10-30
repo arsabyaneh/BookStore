a35df7da1f6fb9c0addeeae194f81bc432d4f889
arsabyaneh <63109912+arsabyaneh@users.noreply.github.com>
2022-10-30 14:55:21 +0100

    added ProductController; used ViewBag and ViewData to pass data from controller to view

---
+++ BookStoreWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,83 @@
+﻿using BookStore.DataAccess.Repository.Contracts;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookStoreWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _unitOfWork.ProductRepository.GetAll();
+
+            return View(products);
+        }
+
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            IEnumerable<SelectListItem> categoryList = _unitOfWork.CategoryRepository.GetAll().Select(x =>
+            {
+                return new SelectListItem()
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                };
+            });
+
+            IEnumerable<SelectListItem> coverTypeList = _unitOfWork.CoverTypeRepository.GetAll().Select(x =>
+            {
+                return new SelectListItem()
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                };
+            });
+
+            if (id == null || id == 0)
+            {
+                Product product = new();
+
+                ViewBag.categoryList = categoryList;
+                ViewData["coverTypeList"] = coverTypeList;
+
+                return View(product);
+            }
+            else
+            {
+                var productFromDb = _unitOfWork.ProductRepository.GetFirstOrDefault(c => c.Id == id);
+
+                if (productFromDb == null)
+                {
+                    return NotFound();
+                }
+
+                return View(productFromDb);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.ProductRepository.Update(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product successfully updated.";
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+    }
+}
